7b5c71347848630411e84f29693b0c96381be02c
miruwu <dragobalan@gmail.com>
2023-04-16 19:31:41 +0200

    namespaces

---
+++ week2/namespaces.py
@@ -0,0 +1,20 @@
+from functii2 import functie2
+
+variabila1 = 2
+a = 3
+b = 2
+msg = 3
+
+def functie_1(a, b):
+    global msg
+    msg = "Hello"
+    variabila1 = a + b
+    return variabila1
+
+
+print(a, b, "rand16")
+suma = functie_1(1, 2)
+print(msg, "rand14")
+print(suma)
+
+functie2(4, 5)
