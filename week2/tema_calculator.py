1084fae8cf2787e2ca0d27db58888958c665d8b5
miruwu <dragobalan@gmail.com>
2023-04-16 20:06:35 +0200

    homework week2 calculator

--- week2/tema_calculator.py
+++ week2/tema_calculator.py
@@ -30,8 +30,11 @@ while True:
         else:
             print(f"Împărțirea celor două numere {operator_1} / {operator_2} = {impartire(int(operator_1), int(operator_2))}")
         break
+    elif (operator_1 == "C") or (operator_2 == "C") or (operatie == "C"):
+        print("Memorie ștearsă. Introdu din nou.")
+        continue
     elif not (operator_1.isdigit() and operator_2.isdigit()):
-        print(f"Calculatorul acceptă doar numere. ")
+        print("Calculatorul acceptă doar numere.")
         break
     else:
         print(f"Alege una dintre aceste operații: {', '.join(['+', '-', '*', '/'])}")
