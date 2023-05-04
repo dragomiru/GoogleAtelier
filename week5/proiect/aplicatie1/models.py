669464ba07b76180cdcb152b807faa7aff90be80
miruwu <dragobalan@gmail.com>
2023-05-04 22:51:13 +0200

    django

---
+++ week5/proiect/aplicatie1/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+
+# Create your models here.
+class Location(models.Model):
+
+    objects = None
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=90)
+    active = models.BooleanField(default=1)
+
+    def __str__(self):
+        return f"{self.country} --> {self.city}"
+
+
+class AuditLocation(models.Model):
+    objects = None
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=90)
+    active = models.BooleanField(default=1)
+    location = models.IntegerField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return f"{self.country} --> {self.city}"
