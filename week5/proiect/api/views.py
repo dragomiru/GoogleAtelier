4de30ab2460ae0b62e195dc135f6b72c1f5e667a
miruwu <dragobalan@gmail.com>
2023-05-11 18:40:40 +0200

    django

---
+++ week5/proiect/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from api.serializers import LocationSerializers
+from aplicatie1.models import Location
+
+
+# Create your views here.
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all().order_by('city')
+    serializer_class = LocationSerializers
---
+++ week5/proiect/user_profile/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Logs(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    action = models.CharField(max_length=10)
+    url = models.CharField(max_length=100)
+
+class Pontaj(models.Model):
+
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(null=True)
